1118968bcd748369ac57fc0a67910645f72bce5d
Jhersy <jhersyvb@gmail.com>
2022-02-04 12:31:14 -0500

    Fetching Userdata from Session

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,8 +2,11 @@ import { Avatar } from '@mui/material'
 import Image from 'next/image'
 import BookmarkOutlinedIcon from '@mui/icons-material/BookmarkOutlined'
 import AddRoundedIcon from '@mui/icons-material/AddRounded'
+import { signOut, useSession } from 'next-auth/react'
 
 function Sidebar() {
+  const { data: session } = useSession()
+
   return (
     <div className="space-y-2 min-w-max max-w-lg">
       {/* Top */}
@@ -13,17 +16,17 @@ function Sidebar() {
         </div>
 
         <Avatar
-          // onClick={signOut}
-          src="https://via.placeholder.com/400"
+          onClick={signOut}
+          src={session?.user?.image}
           className="!h-14 !w-14 !border-2 !absolute !top-4 !cursor-pointer"
         />
 
         <div className="mt-5 py-4 space-x-0.5">
           <h4 className="hover:underline decoration-purple-700 underline-offset-1 cursor-pointer">
-            Jhersy
+            {session?.user?.name}
           </h4>
           <p className="text-black/60 dark:text-white/75 text-sm">
-            jhersyvb@gmail.com
+            {session?.user?.email}
           </p>
         </div>
 
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
+import { getSession, useSession } from 'next-auth/react'
 import Head from 'next/head'
 import Header from '../components/Header'
 import Sidebar from '../components/Sidebar'
+import { useRouter } from 'next/router'
 
 export default function Home() {
+  const router = useRouter()
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      // The user is not authenticated, handle it here.
+      router.push('/home')
+    },
+  })
+
   return (
     <div className="bg-[#f3f2ef] dark:bg-black dark:text-white h-screen overflow-y-scroll md:space-y-6">
       <Head>
@@ -22,4 +33,23 @@ export default function Home() {
       </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  // Check if the user is authenticated on the server
+  const session = await getSession(context)
+  if (!session) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/home',
+      },
+    }
+  }
+
+  return {
+    props: {
+      session,
+    },
+  }
 }
