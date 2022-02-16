801876c8048c6f547dc3e1c130120d4d3ec02f5d
Jhersy <jhersyvb@gmail.com>
2022-02-16 10:26:31 -0500

    Adding extra icons on posts

--- components/Post.js
+++ components/Post.js
@@ -1,19 +1,31 @@
 import { useState } from 'react'
 import { useRecoilState } from 'recoil'
+import { useSession } from 'next-auth/react'
 import { modalState, modalTypeState } from '../atoms/modalAtom'
 import { getPostState } from '../atoms/postAtom'
 import { Avatar, IconButton } from '@mui/material'
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded'
 import MoreHorizRoundedIcon from '@mui/icons-material/MoreHorizRounded'
+import CommentOutlinedIcon from '@mui/icons-material/CommentOutlined'
+import ThumbUpOffAltOutlinedIcon from '@mui/icons-material/ThumbUpOffAltOutlined'
+import ThumbUpOffAltRoundedIcon from '@mui/icons-material/ThumbUpOffAltRounded'
+import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded'
+import ReplyRoundedIcon from '@mui/icons-material/ReplyRounded'
 
 function Post({ post, modalPost }) {
+  const { data: session } = useSession()
   const [modalOpen, setModalOpen] = useRecoilState(modalState)
   const [modalType, setModalType] = useRecoilState(modalTypeState)
   const [postState, setPostState] = useRecoilState(getPostState)
   const [showInput, setShowInput] = useState(false)
+  const [liked, setLiked] = useState(false)
 
   const truncate = (string, n) =>
     string?.length > n ? string.substr(0, n - 1) + '...see more' : string
+
+  const deletePost = () => {
+    //
+  }
 
   return (
     <div
@@ -66,6 +78,42 @@ function Post({ post, modalPost }) {
           }}
         />
       )}
+
+      <div className="flex justify-evenly items-center dark:border-t border-gray-600/80 mx-2.5 pt-2 text-black/60 dark:text-white/75">
+        {modalPost ? (
+          <button className="postButton">
+            <CommentOutlinedIcon />
+            <h4>Comment</h4>
+          </button>
+        ) : (
+          <button
+            className={`postButton ${liked && 'text-blue-500'}`}
+            onClick={() => setLiked(!liked)}
+          >
+            {liked ? (
+              <ThumbUpOffAltRoundedIcon className="-scale-x-100" />
+            ) : (
+              <ThumbUpOffAltOutlinedIcon className="-scale-x-100" />
+            )}
+            <h4>Like</h4>
+          </button>
+        )}
+
+        {session?.user?.email === post.email ? (
+          <button
+            className="postButton focus:text-red-400"
+            onClick={deletePost}
+          >
+            <DeleteRoundedIcon />
+            <h4>Delete post</h4>
+          </button>
+        ) : (
+          <button className="postButton ">
+            <ReplyRoundedIcon className="-scale-x-100" />
+            <h4>Share</h4>
+          </button>
+        )}
+      </div>
     </div>
   )
 }
